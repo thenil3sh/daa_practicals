7c677d79c64f1c914476ef27b2b330fa1f7a7530
thenil3sh <nileshkumar.newdelhi@gmail.com>
2025-02-20 21:09:06 +0530

    now outputs at each and every step

--- practical 1/quick_sort.cpp
+++ practical 1/quick_sort.cpp
@@ -3,19 +3,6 @@
 using namespace std;
 
 template <typename T>
-unsigned int pivot(vector<T>& vec, unsigned int left, unsigned int right) {
-    unsigned int pivot = left;
-    for (unsigned int i = left; i < right; i++) {
-        if (vec[i] < vec[pivot]) {
-            swap(vec[i], vec[pivot + 1]);
-            swap(vec[pivot], vec[pivot + 1]);
-            pivot += 1;
-        }
-    }
-    return pivot;
-}
-
-template <typename T>
 ostream& operator<< (ostream& output, vector<T>& vec){
     output << "[";
     for (unsigned int i = 0; i < vec.size(); i++) {
@@ -25,9 +12,23 @@ ostream& operator<< (ostream& output, vector<T>& vec){
         }
     }
     output << ']';
-
     return output;
 }
+
+template <typename T>
+unsigned int pivot(vector<T>& vec, unsigned int left, unsigned int right) {
+    unsigned int pivot = left;
+    for (unsigned int i = left; i < right; i++) {
+        if (vec[i] < vec[pivot]) {
+            swap(vec[i], vec[pivot + 1]);
+            swap(vec[pivot], vec[pivot + 1]);
+            pivot += 1;
+        }
+    }
+    cout << vec << endl;
+    return pivot;
+}
+
 
 template <typename T>
 void quick_sort(vector<T>& vec, unsigned int left, unsigned int right) {
@@ -37,7 +38,6 @@ void quick_sort(vector<T>& vec, unsigned int left, unsigned int right) {
     unsigned int piv = pivot(vec, left, right);
     quick_sort(vec, left, piv);
     quick_sort(vec, piv + 1, right);
-    cout << vec;
 }
 
 int main() {
